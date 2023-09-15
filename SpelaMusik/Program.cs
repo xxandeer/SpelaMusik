3ec3f15ec7e67216ffbdb7a0e4c3ce57647ef1d1
xxandeer <sjostenalex@gmail.com>
2023-09-15 09:35:57 +0200

    Add project files.

---
+++ SpelaMusik/Program.cs
@@ -0,0 +1,29 @@
+﻿using NAudio.Vorbis;
+using NAudio.Wave;
+using static System.Console;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string oggFilePath = @"99_bottles_of_beer.ogg";
+
+        using (var reader = new VorbisWaveReader(oggFilePath))
+        {
+            using (var waveOut = new WaveOutEvent())
+            {
+                waveOut.Init(reader);
+                waveOut.Play();
+
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+        }
+
+        WriteLine("Tryck på valfri knapp för att avsluta");
+        ReadKey();
+    }
+}
